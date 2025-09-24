cf1ca3c503b7693f6bbe26f7557d98283c2e27f2
sohamphapale <89353652+sohamphapale@users.noreply.github.com>
2025-09-25 04:07:28 +0530

    front and authenticaion pages are done ✅

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const home = () => {
       <div className="bg-cover bg-center  bg-[url(traffic_light.jpg)] h-screen flex justify-between flex-col w-full pt-8">
         <img className="w-16 ml-8" src="RideProwhite1.png" alt="RidePro Logo" />
         <div className="bg-white py-4 pb-7 px-4">
-          <h2 className="text-2xl font-bold">Get Started with Ridepro</h2>
+          <h2 className="text-2xl font-bold">Get Started with Ridepro </h2>
           <Link to="/login" className=" inline-block text-center justify-center w-full bg-black text-white py-3 rounded mt-5">Continue</Link>
         </div>
       </div>
